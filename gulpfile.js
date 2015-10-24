a11bde06343837ec083d4ee30ce3bf77afd0ab81
Joakim Gunst <jgunst@iki.fi>
2015-10-24 12:07:42 +0300

    Updated gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,47 @@
 var gulp = require('gulp');
 var minifyCss = require('gulp-minify-css');
 var concat = require('gulp-concat');
-//var rimraf = require('gulp-rimraf');
+var uglify = require('gulp-uglify');
+var rimraf = require('gulp-rimraf');
 
 var paths = {
-	//scripts: 'public/scripts/*.jsx',
-	css: 'content/css/*.css'
+	css: {
+		src: 'src/css/*.css',
+		dest: 'public/css'
+	},
+	js: {
+		src: 'src/js/*.js',
+		dest: 'public/js'
+	}
 };
 
-gulp.task('default', ['css'])
+gulp.task('default', ['css', 'js'])
 
-gulp.task('css', function () {
-	return gulp.src(paths.css)
+gulp.task('watch', function() {
+	gulp.watch(paths.css.src, ['css']);
+	gulp.watch(paths.js.src, ['js']);
+});
+
+gulp.task('css', ['clean-css'], function () {
+	return gulp.src(paths.css.src)
 		.pipe(minifyCss())
 		.pipe(concat('main.min.css'))
-		.pipe(gulp.dest('public/css'))
+		.pipe(gulp.dest(paths.css.dest))
 });
 
-/*gulp.task('scripts', function () {
-	return gulp.src(paths.scripts)
+gulp.task('js', ['clean-js'], function () {
+	return gulp.src(paths.js.src)
 		.pipe(uglify())
-		.pipe(concat('app.js'))
-		.pipe(gulp.dest('public/build'))
+		.pipe(concat('main.min.js'))
+		.pipe(gulp.dest(paths.js.dest))
 });
-*/
-/*gulp.task('watch', function() {
-	//gulp.watch(paths.scripts, ['scripts']);
-});*/
+
+gulp.task('clean-css', function () {
+	return gulp.src(paths.css.dest, { read: false })
+		.pipe(rimraf());
+});
+
+gulp.task('clean-js', function () {
+	return gulp.src(paths.js.dest, { read: false })
+		.pipe(rimraf());
+});
