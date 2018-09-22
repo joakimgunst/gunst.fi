410858992ee612fd1554a87a1b8e6aa6946a2b5f
Joakim Gunst <joakim.gunst@gmail.com>
2018-09-22 13:29:38 +0300

    Added manifest and offline support.

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,19 @@ module.exports = {
     'gatsby-plugin-sass',
     'gatsby-plugin-react-svg',
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Joakim Gunst`,
+        short_name: `Joakim Gunst`,
+        start_url: `/`,
+        background_color: `#4b4e50`,
+        theme_color: `#4b4e50`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`
+      }
+    },
+    'gatsby-plugin-offline',
+    {
       resolve: 'gatsby-plugin-google-fonts',
       options: {
         fonts: ['Alegreya Sans:400,400i,500,700']
